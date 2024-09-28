415bfb2f6806de9b0840942dc9a8d530861f4cfe
DucLich <lich04d3@gmail.com>
2024-09-28 14:11:55 +0700

    My Love

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+  introTitle: "Babe à!",
+  introDesc: `Yêu một người không khó
+    Vì chẳng cần lý do
+    Chút tình cảm nho nhỏ
+    Chỉ dành cho em đó`,
+  btnIntro: "Next!",
+  title: "Làm người yêu anh nhé 😙!!",
+  desc: "Không biết mk đã yêu từ khi nào.Cũng không biết mk yêu đôi mắt, khuôn mặt hay vóc dáng đó. Chỉ biết rằng mình yêu con người đó mất rồi. ",
+  btnYes: "Vâng anh yêu 🥰 <33",
+  btnNo: "Không, Anh trai à :3",
+  question: "Cảm ơn em! Em yêu muốn nói gì với anh nào... <3",
+  btnReply: "Gửi cho anh <3",
+  reply: "Yêu thì yêu, mà không yêu thì yêu <33333333",
+  mess: "Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘",
+  messDesc: "Tối nay 7h anh qua đón nhé công chúa.",
+  btnAccept: "Okiiiii lun <3",
+};
